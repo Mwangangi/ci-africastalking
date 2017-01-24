603fc842941e56e56962cb871738d1eab3c29d7e
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2017-01-24 10:30:17 +0300

    Moved AfricastalkingGateway.php class to third party. To add a library that extends the class It always a good not to modify the parent class for ease in maintenance :-)

--- application/config/africastalking.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config = array(
-    'username' => 'your_africastalking_username',
-    'apiKey' => 'your_africastalking_api_key'
-);
